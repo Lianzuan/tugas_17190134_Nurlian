049ce44be62034cd205cba76107113a52ce3d797
Lianzuan <101781548+Lianzuan@users.noreply.github.com>
2022-04-04 15:43:57 +0700

    Add files via upload

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+Class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Lian zuan ";
+	}
+}
+?>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>latihan 1</title>
+</head>
+<body>
+Halo kawan.. yuk kita belajar web programming..!!<br>
+Nilai 1 = <?= $nilai1; ?>
+Nilai 2 = <?= $nilai2; ?>
+Ini hasil dari pemodelan methode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+</html>
