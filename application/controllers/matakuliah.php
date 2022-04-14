748fafe64709cef7bbfbb6eb308eaa71a43b1ef1
Lianzuan <101781548+Lianzuan@users.noreply.github.com>
2022-04-14 07:47:52 +0700

    form matakuliah

---
+++ application/controllers/matakuliah.php
@@ -0,0 +1,32 @@
+<?php
+	class Matakuliah extends CI_Controller
+	{
+		public function index()
+		{
+			$this->load->view('view-form-matakuliah');
+		}
+		
+		public function cetak()
+		{
+			$this->form_validation->set_rules('kode', 'Kode', 'required', 
+			array('required' => '%s harus diisi.')
+			);
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('sks', 'SKS', 'required');
+			
+			if ($this->form_validation->run() == FALSE)
+			{
+				$this->load->view('view-form-matakuliah');
+			}
+			else
+			{
+				$data = [
+				'kode' => $this->input->post('kode'),
+				'nama' => $this->input->post('nama'),
+				'sks' => $this->input->post('sks')
+				];
+				
+				$this->load->view('view-data-matakuliah', $data);
+			}
+		}
+	}
